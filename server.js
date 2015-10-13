2c858fdb5e5cb25b37817dd179e50d100f6e3326
Susan <susan.adelokiki@andela.co>
2015-10-13 22:26:25 +0100

    Run node server

---
+++ server.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var app = require('express')();
+var model = require('./schema');
+var Sequelize = require('sequelize');
+
+
+model.sequelize.sync({
+  force: true
+}).then(function() {
+  app.listen(4000, function() {
+    console.log("done");
+  });
+}).then(function(err) {
+  console.log('error', err);
+});
