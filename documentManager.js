2345d570faf366aa01211a3c49e1999d6e28dae2
Susan <susan.adelokiki@andela.co>
2015-10-13 22:44:52 +0100

    Write function to pass failing test

--- documentManager.js
+++ documentManager.js
@@ -8,3 +8,99 @@ var User = require('./schema').users,
     dialect: 'postgres',
     port: 5432,
   });
+
+module.exports = {
+
+  createUser: function(first, last, title) {
+    var user, roleName;
+    user = {
+      firstname: first,
+      lastname: last,
+      UserRole: title
+    }
+    return Role.findOrCreate({
+      where: {
+        title: title
+      }
+    }).then(function() {
+      User.findOrCreate({
+        where: {
+          firstname: first,
+          lastname: last,
+          UserRole: title
+        }
+      });
+    });
+  },
+
+  getAllUsers: function() {
+    return User.findAll();
+  },
+
+  createRole: function(title) {
+    return Role.create({
+      title: title
+    });
+  },
+
+  getAllRoles: function() {
+    return Role.findAll({
+      where: {}
+    });
+  },
+
+  createDocument: function(title, role, userFirstName, userLastName) {
+    var dateCreated = new Date();
+    var currentDate = dateCreated.getDate();
+    var currentMonth = dateCreated.getMonth();
+    var currentYear = dateCreated.getFullYear();
+    var publishedOn = currentDate + '-' + currentMonth + '-' + currentYear;
+
+    return Role.findOrCreate({
+      where: {
+        title: role
+      }
+    }).then(function() {
+      User.findOrCreate({
+        where: {
+          firstname: userFirstName,
+          lastname: userLastName,
+          UserRole: role
+        }
+      });
+    }).then(function() {
+      Document.create({
+        title: title,
+        createdOn: publishedOn,
+        documentRole: role
+      });
+    });
+  },
+
+  getAllDocuments: function(limit) {
+    return Document.findAll({
+      limit: limit,
+      order: '"createdAt" DESC'
+    });
+  },
+
+  getAllDocumentsByRole: function(role, limit) {
+    return Document.findAll({
+      where: {
+        documentRole: role,
+      },
+      limit: limit,
+      order: '"createdAt" DESC'
+    });
+  },
+
+  getAllDocumentsByDate: function(date, limit) {
+    return Document.findAll({
+      where: {
+        createdOn: date
+      },
+      limit: limit
+    });
+  }
+
+};
