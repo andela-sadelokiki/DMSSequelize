03fe6f8c09aa78197fc535a6d5045df689de62eb
Susan <susan.adelokiki@andela.co>
2015-10-13 22:29:24 +0100

    Create document schema

---
+++ schema/document.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = function(sequelize, DataTypes) {
+  var Document = sequelize.define('documents', {
+    title: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false
+    },
+    createdOn: {
+      type: DataTypes.STRING
+    }
+  }, {
+    classMethods: {
+      associate: function(models) {
+        Document.belongsTo(models.roles, {
+          foreignKey: 'documentRole',
+          targetKey: 'title',
+          onDelete: 'SET NULL',
+          onUpdate: 'CASCADE'
+        });
+      },
+    }
+  });
+  return Document;
+};
